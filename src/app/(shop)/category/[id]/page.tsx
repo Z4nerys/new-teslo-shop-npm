7a6b7947c51ce2162c121c6595b93afb56422ceb
zanerys <gmfkratze@gmail.com>
2023-12-22 17:07:50 -0300

    estructura de proyecto

---
+++ src/app/(shop)/category/[id]/page.tsx
@@ -0,0 +1,9 @@
+
+export default function () {
+    return (
+      <div>
+          <h1>Category page</h1>
+      </div>
+    )
+  }
+  
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,7 +3,7 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <h1>Hola Mundo</h1>
       <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
     </div>
