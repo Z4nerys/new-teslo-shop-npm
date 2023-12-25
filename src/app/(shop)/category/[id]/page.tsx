4d998bc1ddc2b9fb8ecfd1c6065f2f9ad8b1d3d6
zanerys <gmfkratze@gmail.com>
2023-12-25 19:25:35 -0300

    .

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,9 +1,23 @@
+import { notFound } from "next/navigation";
 
-export default function () {
-    return (
-      <div>
-          <h1>Category page</h1>
-      </div>
-    )
+interface Props {
+  params: {
+    id: string;
   }
-  
+}
+
+export default function ({ params }: Props) {
+
+  const { id } = params;
+
+  if( id === 'kids') {
+    notFound();
+  }
+
+  return (
+    <div>
+      HACER EL 404 personalizado. video 193
+      <h1>Category page {id}</h1>
+    </div>
+  )
+}
