660738eb7bb0545524e67b20229128af5ae1f097
zanerys <gmfkratze@gmail.com>
2023-12-28 18:58:54 -0300

    finish primer seccion

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,47 +1,139 @@
 'use client'
+import Link from "next/link"
+import clsx from "clsx"
+
+import { useUIStore } from "@/store"
 //esto es x el onClick, evento del cliente
-import { IoCloseOutline, IoSearchOutline } from "react-icons/io5"
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
 
 
 export const Sidebar = () => {
-  return (
-    <div>
-        {/* background black  */}
-        <div
-            className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30 "
-        />
 
-        {/* BLUR */}
-        <div
-            className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"
-        />
+    //si uso un hook tengo que poner que es 'use client'
+    const isSideMenuOpen = useUIStore(state => state.isSideMenuOpen)
+    const closeMenu = useUIStore(state => state.closeSideMenu)
 
-        {/* sideMenu */}
-        <nav
-            //todo: efecto de slide
-            className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300"
-        >
-            
-            <IoCloseOutline
-                size={ 50 }
-                className="absolute top-5 right-5 cursor-pointer"
-                onClick={() => console.log('click')} //evento del cliente
-            />
+    return (
+        <div>
+            {/* background black  */}
 
-            {/* input */}
+            {
+                isSideMenuOpen && (
+                    <div
+                        className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30 "
+                    />
 
-            <div className="relative mt-14">
-                <IoSearchOutline
-                    size={ 20 }
-                    className="absolute top-2 left-2"
+                )
+            }
+
+            {/* BLUR */}
+            {
+                isSideMenuOpen && (
+                    <div
+                        onClick={closeMenu}
+                        className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"
+                    />
+                )
+            }
+
+
+            {/* sideMenu */}
+            <nav
+                className={
+                    clsx(
+                        "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+                        {
+                            "translate-x-full": !isSideMenuOpen
+                        }
+                    )
+                }
+            >
+
+                <IoCloseOutline
+                    size={50}
+                    className="absolute top-5 right-5 cursor-pointer"
+                    onClick={closeMenu} //evento del cliente
                 />
-                <input
-                    type="text"
-                    placeholder="Buscar"
-                    className="w-full bg-gray-100 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
-                />
-            </div>
-        </nav>
-    </div>
-  )
+
+                {/* input */}
+
+                <div className="relative mt-14">
+                    <IoSearchOutline
+                        size={20}
+                        className="absolute top-2 left-2"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Buscar"
+                        className="w-full bg-gray-100 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
+                    />
+                </div>
+
+                {/* Menú */}
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoPersonOutline size={30} />
+                    <span className="ml-3 text-lg">Perfil</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoTicketOutline size={30} />
+                    <span className="ml-3 text-lg">Ordenes</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoLogInOutline size={30} />
+                    <span className="ml-3 text-lg">Ingresar</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoLogOutOutline size={30} />
+                    <span className="ml-3 text-lg">Salir</span>
+                </Link>
+
+
+                {/* line Separator */}
+                <div className="w-full h-0.5 bg-gray-200 my-10 rounded-md" />
+
+
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoShirtOutline size={30} />
+                    <span className="ml-3 text-lg">Productos</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoTicketOutline size={30} />
+                    <span className="ml-3 text-lg">Ordenes</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoPeopleOutline size={30} />
+                    <span className="ml-3 text-lg">Usuarios</span>
+                </Link>
+
+            </nav>
+        </div>
+    )
 }
