9362e40b8fa54f560c4c986d853dd59a1b1a164f
Gasty <gmfkratze@gmail.com>
2024-02-14 23:09:34 -0300

    .

--- src/app/(shop)/cart/ui/OrderSummary.tsx
+++ src/app/(shop)/cart/ui/OrderSummary.tsx
@@ -1,16 +1,30 @@
 'use client'
 
 import { useEffect, useState } from "react"
+import { useRouter } from 'next/navigation';
 import Link from "next/link"
+
 import { useCartStore } from "@/store"
+import { currencyFormat } from '../../../../utils';
 
 export const OrderSummary = () => {
+    const router = useRouter();
+
     const [loaded, setLoaded] = useState(false)
 
-    const {itemsInCart, subTotal, tax, total} = useCartStore(state => state.getSummaryInformation())
+    const { itemsInCart, subTotal, tax, total } = useCartStore(state => state.getSummaryInformation())
+    
     useEffect(() => {
         setLoaded(true)
     }, [])
+
+    useEffect(() => {
+
+        if ( itemsInCart === 0 && loaded === true )   {
+          router.replace('/empty')
+        }
+    
+      },[ itemsInCart, loaded ])
 
     if (!loaded) return <p>Loading...</p>
 
@@ -18,16 +32,16 @@ export const OrderSummary = () => {
         <>
             <div className="grid grid-cols-2">
                 <span>Nro. Productos</span>
-                <span className="text-right">{itemsInCart === 1 ? '1 articulo': `${itemsInCart} articulos`}</span>
+                <span className="text-right">{itemsInCart === 1 ? '1 articulo' : `${itemsInCart} articulos`}</span>
 
                 <span>Subtotal</span>
-                <span className="text-right">{subTotal}</span>
+                <span className="text-right">{currencyFormat(subTotal)}</span>
 
                 <span>Impuestos (15%)</span>
-                <span className="text-right">{tax}</span>
+                <span className="text-right">{currencyFormat(tax)}</span>
 
                 <span className="mt-5 text-2xl">Total:</span>
-                <span className="mt-5 text-2xl text-right">{total}</span>
+                <span className="mt-5 text-2xl text-right">{currencyFormat(total)}</span>
 
             </div>
             <div className="mt-5 mb-2 w-full">
